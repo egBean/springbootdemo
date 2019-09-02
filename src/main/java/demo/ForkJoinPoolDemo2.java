8730f5acfefcbb4eca0e258f7179d1d3a441c3f6
egbean <1358404767@qq.com>
2019-09-02 17:41:39 +0800

    增加一个forkjoinpool demo

---
+++ src/main/java/demo/ForkJoinPoolDemo2.java
@@ -0,0 +1,50 @@
+package demo;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ForkJoinPoolDemo2 {
+    public static void main(String[] args) {
+        long[] numbers = new long[]{1,2,4,5,6,4};
+        System.out.println(new ForkJoinPool().invoke(new ForkJoinSumCalculator(numbers)));;
+    }
+}
+
+class ForkJoinSumCalculator extends RecursiveTask<Long>{
+
+    private final long[] numbers;
+    private final int start;
+    private final int end;
+
+    private final static int LIMIT = 10_000;
+
+    public ForkJoinSumCalculator(long[] numbers){
+        this(numbers,0,numbers.length);
+    }
+
+    private ForkJoinSumCalculator(long[] numbers,int start,int end){
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+    }
+    @Override
+    protected Long compute() {
+        if((end-start)<LIMIT){
+            return calculate();
+        }
+        ForkJoinSumCalculator left = new ForkJoinSumCalculator(numbers,start,start+end/2);
+        left.fork();
+        ForkJoinSumCalculator right = new ForkJoinSumCalculator(numbers,start+end/2,end);
+        Long rightResult = right.compute();
+        Long leftResult = left.join();
+        return rightResult+leftResult;
+    }
+
+    private long calculate(){
+        long result = 0;
+        for(int i = start;i<end;i++){
+            result += numbers[i];
+        }
+        return result;
+    }
+}
