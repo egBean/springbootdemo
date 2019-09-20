38e604c79fd4ceb3c129162e52ab72a6001b157b
egbean <1358404767@qq.com>
2019-09-20 20:18:43 +0800

    对jmstemplate进行配置。

--- src/main/java/bootstrap/activemq/ConfigBean.java
+++ src/main/java/bootstrap/activemq/ConfigBean.java
@@ -4,8 +4,9 @@ import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
-
+import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 
 @Component
@@ -18,5 +19,17 @@ public class ConfigBean {
     @Bean
     public Queue queue1(){
         return new ActiveMQQueue(queue1);
+    }
+
+    @Bean
+
+    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+        JmsTemplate template = new JmsTemplate(connectionFactory);
+        template.setPubSubDomain(false);
+        template.setDeliveryMode(2);
+        template.setConnectionFactory(connectionFactory);
+        template.setSessionAcknowledgeMode(2);
+        template.setSessionTransacted(false);
+        return template;
     }
 }
