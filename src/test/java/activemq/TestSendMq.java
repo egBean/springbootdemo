2255d9e9bdad4f25ec2327d6eb9a6f3fe0222925
egbean <1358404767@qq.com>
2019-10-11 18:08:44 +0800

    测试类基本使用

--- src/test/java/activemq/TestSendMq.java
+++ src/test/java/activemq/TestSendMq.java
@@ -6,24 +6,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.annotation.Resource;
 
 @SpringBootTest(classes = Example.class)
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+//@WebAppConfiguration
 public class TestSendMq {
 
-    @Resource
-    private MqSender mqSender;
+    /*@Resource
+    private MqSender mqSender;*/
 
     public static void main(String[] args) {
 
     }
     @Test
     public void sendMsg(){
-        mqSender.produceMsg("laownag 2hao aaa");
-        System.out.println("-----------------------------");
+        System.out.println("zzzz");
     }
 }
