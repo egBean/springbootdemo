0753c1d26882828f9d589c0042b8553a425b8322
egbean <1358404767@qq.com>
2019-11-01 17:31:19 +0800

    增加一个netty demo。

---
+++ src/test/java/test/netty/TimeServer.java
@@ -0,0 +1,58 @@
+package test.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+    public static void main(String[] args) throws Exception {
+        NioEventLoopGroup bossGroup =new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors());
+        try{
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                                @Override
+                                public void channelActive(final ChannelHandlerContext ctx) { // (1)
+                                    final ByteBuf time = ctx.alloc().buffer(4); // (2)
+                                    time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+
+                                    final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+                                    f.addListener(new ChannelFutureListener() {
+                                        @Override
+                                        public void operationComplete(ChannelFuture future) {
+                                            assert f == future;
+                                            ctx.close();
+                                        }
+                                    }); // (4)
+                                }
+
+                                @Override
+                                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                                    cause.printStackTrace();
+                                    ctx.close();
+                                }
+                            });
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6);
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = server.bind(8888).sync(); // (7)
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+            } finally {
+                workerGroup.shutdownGracefully();
+                bossGroup.shutdownGracefully();
+            }
+    }
+}
